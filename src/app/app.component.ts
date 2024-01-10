0f75a8d965158f9bc6d1b8511487b14f88731770
Steven Ragy <stevenragy@github.com>
2024-01-10 10:43:03 +0200

    lesson 5: tempaltes,interpolation and directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from './products/product-list.component';
 
 @Component({
   selector: 'pm-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
